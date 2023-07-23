299ba947a16b203d8f68a3caafbcbf21f5b274dd
SakshiMore2215 <127644721+SakshiMore2215@users.noreply.github.com>
2023-07-23 22:16:48 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,34 @@
+
+let allMusic = [
+    {
+        name: "LIGHT SWITCH",
+        artist: "CHARLIE PUTH",
+        img: "music1",
+        src: "music1"
+    },
+    {
+        name: "That That",
+        artist: "BTS-Suga,Psy",
+        img: "music2",
+        src: "music2"
+    },
+    {
+        name: "Left and Right",
+        artist: "Jungkook,Charlie Puth",
+        img: "music3",
+        src: "music3"
+    },
+    {
+        name: "SHAPE OF YOU",
+        artist: "ED SHEERAN",
+        img: "music4",
+        src: "music4"
+    },
+    {
+        name: "NIGHT CHANGES",
+        artist: "ONE DIRECTION",
+        img: "music5",
+        src: "music5"
+    },
+     
+   ];
